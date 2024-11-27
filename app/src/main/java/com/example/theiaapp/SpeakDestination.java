886a9e67b66c16164466b61a3fd922a0c1b336a8
Sierra Pine <81993904+sierratp@users.noreply.github.com>
2024-11-26 22:48:55 -0800

    fixes

--- app/src/main/java/com/example/theiaapp/SpeakDestination.java
+++ app/src/main/java/com/example/theiaapp/SpeakDestination.java
@@ -2,12 +2,7 @@ package com.example.theiaapp;
 
 import android.os.Bundle;
 import android.widget.TextView;
-
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
 public class SpeakDestination extends AppCompatActivity {
 
